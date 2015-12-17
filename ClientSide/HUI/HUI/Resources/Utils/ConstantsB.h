70aa45ede8bba1082b178160abe4a0a60e87d11f
JOAQUIN GIRALDEZ LOPEZ <JOAQUING@HANZO.ES>
2015-12-17 17:35:53 +0100

    test

---
+++ ClientSide/HUI/HUI/Resources/Utils/ConstantsB.h
@@ -0,0 +1,13 @@
+//
+//  ConstantsB.h
+//  HUI
+//
+//  Created by Joaquin Giraldez on 17/12/15.
+//  Copyright © 2015 Giraldez Lopez SL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConstantsB : NSObject
+
+@end
