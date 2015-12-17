3b888560bb2da05ee6bdf17d077d1bb88885e16d
JOAQUIN GIRALDEZ LOPEZ <JOAQUING@HANZO.ES>
2015-12-17 08:50:01 +0100

    define constants

---
+++ ClientSide/HUI/HUI/Resources/Utils/Constants.h
@@ -0,0 +1,9 @@
+//
+//  Constants.h
+//  HUI
+//
+//  Created by Joaquin Giraldez on 17/12/15.
+//  Copyright © 2015 Giraldez Lopez SL. All rights reserved.
+//
+
+#define KEY_VOICE_RECOGNITION @"xxx"
