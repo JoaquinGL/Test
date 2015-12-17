53cc6ecbb16a9904f44725e90e58b65f52ae8148
JOAQUIN GIRALDEZ LOPEZ <JOAQUING@HANZO.ES>
2015-12-17 09:16:20 +0100

    test

---
+++ ClientSide/HUI/HUI/Resources/Utils/ConstantsB.h
@@ -0,0 +1,9 @@
+//
+//  ConstantsB.h
+//  HUI
+//
+//  Created by Joaquin Giraldez on 17/12/15.
+//  Copyright © 2015 Giraldez Lopez SL. All rights reserved.
+//
+
+#define KEY_VOICE_RECOGNITION @"xxx"
